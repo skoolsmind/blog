b35a6b56e97c48c8cec157f9e21841b5e05b2056
amr sharkas <amrsharkas@amrs-MacBook-Pro.local>
2022-05-04 03:24:39 +0300

    resturcture view files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,47 @@
 <x-layout>
 
-        @foreach($posts as $post)
-                <article class="@if($loop->last || $loop->odd) mb-{{$loop->index}} @endif">
-                    <h1>
-                        <a href="/post/{{$post->slug}}">
-                            {{$post->title}}
-                        </a>
+    @include('_posts-header')
 
-                    </h1>
-                    <p>
-                        By <a href="/authors/{{$post->author->username}}">
-                            {{$post->author->name}}
-                        </a>
-                        in
-                        <a href="/categories/{{$post->category->slug}}">
-                            {{$post->category->name}}
-                        </a>
-                    </p>
-                    <div>
-                        <p>{{$post->excerpt}}</p>
-                    </div>
-                </article>
-            @endforeach
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
+
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
+
+
+        </div>
+    </main>
+
+{{--        @foreach($posts as $post)--}}
+{{--                <article class="@if($loop->last || $loop->odd) mb-{{$loop->index}} @endif">--}}
+{{--                    <h1>--}}
+{{--                        <a href="/post/{{$post->slug}}">--}}
+{{--                            {{$post->title}}--}}
+{{--                        </a>--}}
+
+{{--                    </h1>--}}
+{{--                    <p>--}}
+{{--                        By <a href="/authors/{{$post->author->username}}">--}}
+{{--                            {{$post->author->name}}--}}
+{{--                        </a>--}}
+{{--                        in--}}
+{{--                        <a href="/categories/{{$post->category->slug}}">--}}
+{{--                            {{$post->category->name}}--}}
+{{--                        </a>--}}
+{{--                    </p>--}}
+{{--                    <div>--}}
+{{--                        <p>{{$post->excerpt}}</p>--}}
+{{--                    </div>--}}
+{{--                </article>--}}
+{{--            @endforeach--}}
 
 </x-layout>
